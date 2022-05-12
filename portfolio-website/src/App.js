5cf4cffcd5dcb281e95abd83257f5fb5abd000b1
Cholwe Muleya <cholwemuleya1@gmail.com>
2022-05-12 09:21:15 +0200

    Modifications

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,10 +1,10 @@
-
+import Profile from './PortfolioContainer/Home/Profile';
 import './App.css';
 
  function App () {
   return (
     <div className="App">
-      
+      <Profile />
     </div>
   );
 }
