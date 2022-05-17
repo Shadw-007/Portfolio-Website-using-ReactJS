f6b807ef575fa1037c8775557e104a61b560e56e
Cholwe Muleya <cholwemuleya1@gmail.com>
2022-05-17 07:42:04 +0200

    website creation

--- portfolio-website/src/PortfolioContainer/Home/Profile.js
+++ portfolio-website/src/PortfolioContainer/Home/Profile.js
@@ -1,44 +1,81 @@
-import React from 'react'
+import React from "react";
+import Typical from 'react-typical';
 
 export default function Profile() {
   return (
-    <div className='profile-container'>
-        <div className='profile-parent'>
-            <div className='profile-details'>
-                <div className='colz'>
-                     <a href='#'>
-                         <i className='fa-facebook-square'></i>
-                     </a>
-                     <a href='#'>
-                         <i className='fa-google-plus-square'></i>
-                     </a>
-                     <a href='#'>
-                         <i className='fa-instagram'></i>
-                     </a>
-                     <a href='#'>
-                         <i className='fa-linkedin-square'></i>
-                     </a>
-                     <a href='#'>
-                         <i className='fa-twitter'></i>
-                     </a>
-                </div>
-            
-            <div className='profile-details-name'>
-                <span className='primary-text'>
-                    {' '}
-                    Hello, WE'RE <span className='highlighted-text'>Cholwe and Mapalo</span>
-                </span>
-                <div className='profile-details-role'>
-                    <span className='primary-text'>
-                        {' '}
-                        <h1>
-                            
-                        </h1>
-                    </span>
-                </div>
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className='colz-icon'>
+            <a href="#">
+              <i className="fa-facebook-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa-google-plus-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i className="fa-linkedin-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa-twitter"></i>
+            </a>
             </div>
-            </div>
+          </div>
+
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, WE'RE{" "}
+              <span className="highlighted-text">Cholwe and Mapalo</span>
+            </span>
+          </div>
+          <div className='profile-details-role'>
+              {/* <span className='primary-text'>
+                  {" "}
+                  <h1>
+                     {" "} 
+                     <Typical
+                     loop={Infinity}
+
+                     steps={[
+                       "Enthusiastic Devs 🛑",
+                       1000,
+                       "Full-Stack Developers 🌐",
+                       1000,
+                       "Creative Designers ✍🏾",
+                       1000,
+                       "Database Creators 💻",
+                       1000,
+                       "Database Analysts 👨🏾‍💻",
+                       1000,
+                     ]}
+                     />
+                  </h1>
+                <span className='profile-role-tagline'>
+              Knack of building applications with front and back-end operations
+            </span>  
+                </span> */}
+          </div>
+          <div className='profile-options'>
+            <button className='btn primary-btn'>
+              {""}
+              Hire Me
+            </button>
+            <a href='ehizcv.pdf' download='Ehidedu ehizcv.pdf'>
+              <button className="btn highlighted-btn">Download CV</button>
+            </a>
+          </div>
         </div>
+        <div className='profile-picture'>
+          <div className='profile-picture-background'>
+
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
